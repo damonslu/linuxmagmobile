0f3eb87fc7e346356615444888f7b0650e233d1f
frank.ableson <frank.ableson@6b24715a-5871-11de-9579-adebd1cebf4d>
2009-08-17 19:36:09 +0000

    Checking in code

---
+++ AndroidApplication_TicketResponder/src/com/msi/linuxmagazine/ticketing/TicketResponder.java
@@ -0,0 +1,85 @@
+/*
+ * 
+ * 
+ * TicketResponder.java
+ * Ticketing sample application written for Linux Magazine
+ * Author: Frank Ableson, fableson@msiservices.com
+ * More details: http://www.linux-mag.com/blogs/fableson
+ * August 2009
+ * 
+ */
+package com.msi.linuxmagazine.ticketing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ImageView;
+import android.util.Log;
+import android.app.AlertDialog;
+
+
+public class TicketResponder extends Activity {
+	
+	
+	final String tag = "TicketResponder";
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+        final EditText ticketNumber = (EditText) findViewById(R.id.TicketNumber);
+        final Spinner ticketStatus = (Spinner) findViewById(R.id.TicketStatus);
+        final Button gobutton = (Button) findViewById(R.id.GoButton);
+        gobutton.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+                try {
+                	String ticket = ticketNumber.getText().toString();
+                	String status = ticketStatus.getSelectedItem().toString();
+                	Log.i(tag,"Ticket Number is [" + ticket + "]");
+                	if (ticket.trim().length() == 0)
+                	{
+                		AlertDialog.Builder adb = new AlertDialog.Builder(TicketResponder.this);
+                        AlertDialog ad = adb.create();
+                        ad.setMessage("Ticket number required.");
+                        ad.show();
+                	}
+                	else
+                	{
+                		AlertDialog.Builder adb = new AlertDialog.Builder(TicketResponder.this);
+                        AlertDialog ad = adb.create();
+                        ad.setMessage("Updating ticket (" + ticket.trim() + ") to (" + status + ")");
+                        ad.show();
+                	}
+                	
+                } catch (Exception e) {
+                    Log.i(tag, "Failed to process request [" + e.getMessage() + "]");
+                }
+            }
+        });
+        
+        final ImageView logo = (ImageView) findViewById(R.id.ImageView02);
+        logo.setOnLongClickListener(new ImageView.OnLongClickListener() {
+        	public boolean onLongClick(View v)
+        	{
+        		
+                Intent launchLM = new Intent("android.intent.action.VIEW", android.net.Uri.parse("http://linux-mag.com/blogs/fableson"));
+                startActivity(launchLM);
+
+        		return true;
+        	}
+        }
+        );
+
+        
+    }
+}
